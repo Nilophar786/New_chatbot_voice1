3ad8955c374df8ccc6da2dc3b985f2e14324ff6e
Nilophar786 <Shaikhnilofar0027@gmail.com>
2026-02-05 14:52:57 +0530

    Update cookie settings for token in auth controller

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -51,8 +51,8 @@ export const signUp=async (req,res)=>{
         res.cookie("token",token,{
             httpOnly:true,
             maxAge:7*24*60*60*1000,
-            sameSite:"strict",
-            secure: process.env.NODE_ENV === 'production'
+            sameSite:"None",
+            secure:true
         })
 
         console.log(`User created successfully: ${user.email}`)
